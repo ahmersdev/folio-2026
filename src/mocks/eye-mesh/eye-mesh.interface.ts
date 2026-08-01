b8a3c573f6dcfa7f0475dce70d03387770153d1a
ahmersdev <mahmerakram6@gmail.com>
2026-08-01 23:56:21 +0500

    cleanup

--- src/mocks/eye-mesh/eye-mesh.interface.ts
+++
@@ -1,5 +0,0 @@
-type TPointerTarget = { x: number; y: number };
-
-export interface IEyeProps {
-  pointer: React.RefObject<TPointerTarget>;
-}
--- src/mocks/icons/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as ClosedEye } from "./closed-eye-icon";
-export { default as SharinganEye } from "./sharingan-eye-icon";
